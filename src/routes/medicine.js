390c1d79688ab63009745bf5e8a172dd254a4637
arleysouza <arley.ferreira.souza@gmail.com>
2022-05-23 15:49:18 -0300

    post list

--- src/routes/medicine.js
+++ src/routes/medicine.js
@@ -5,7 +5,7 @@ const {create, remove, update, list} = new MedicineController();
 router.post("/create", create);
 router.delete("/remove", remove);
 router.put("/update", update);
-router.get("/list", list);
+router.post("/list", list);
 
 router.use( (_, res) => {
 	res.status(200).json({error:"Operação desconhecida com o medicamento"});
